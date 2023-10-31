0a4fd101a230cfea3e67876e649c849c2306166b
HappyLemon <sm.itsmyworld@gmail.com>
2023-10-31 22:19:12 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * Написать программу для вывода «Hello <user>» на консоль. Пусть <user>
+ * вводиться с консоли, программа должна потребовать ввод <user>.
+ * Для чтения с консоли используйте Console.ReadLine().
+ */
+Console.WriteLine("Введите имя пользователя");
+var userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}!");
